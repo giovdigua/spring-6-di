b439c04ae9306ca36c16dbed31a2502f7999ee6a
Giovanni DI GUARDO <g.diguardo@cineca.it>
2024-05-05 18:08:22 +0200

    Spring Profiles

---
+++ src/main/java/com/giovdigua/spring6di/services/i18n/EnglishGreetingServiceImpl.java
@@ -0,0 +1,14 @@
+package com.giovdigua.spring6di.services.i18n;
+
+import com.giovdigua.spring6di.services.GreetingService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+@Profile("EN")
+@Service("i18NService")
+public class EnglishGreetingServiceImpl implements GreetingService {
+    @Override
+    public String sayGreeting() {
+        return "Hello World - EN";
+    }
+}
---
+++ src/main/java/com/giovdigua/spring6di/services/i18n/SpanishGreetingServiceImpl.java
@@ -0,0 +1,14 @@
+package com.giovdigua.spring6di.services.i18n;
+
+import com.giovdigua.spring6di.services.GreetingService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+@Profile("ES")
+@Service("i18NService")
+public class SpanishGreetingServiceImpl implements GreetingService {
+    @Override
+    public String sayGreeting() {
+        return "Hola Mundo - ES";
+    }
+}
