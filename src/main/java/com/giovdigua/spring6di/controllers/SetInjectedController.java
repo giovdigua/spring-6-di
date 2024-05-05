80cc05ae354df66df650909b105fb24fefad2ba9
Giovanni DI GUARDO <g.diguardo@cineca.it>
2024-05-05 17:51:44 +0200

    Using Qualifiers

--- src/main/java/com/giovdigua/spring6di/controllers/SetInjectedController.java
+++ src/main/java/com/giovdigua/spring6di/controllers/SetInjectedController.java
@@ -2,6 +2,7 @@ package com.giovdigua.spring6di.controllers;
 
 import com.giovdigua.spring6di.services.GreetingService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 
 @Controller
@@ -10,6 +11,7 @@ public class SetInjectedController {
 
     private GreetingService greetingService;
 
+    @Qualifier("setterGreetingBean")
     @Autowired
     public void setGreetingService(GreetingService greetingService) {
         System.out.println("SetInjectedController.setGreetingService");
