65e59065380cd41855f35bec1aa6944c80cf8bfb
Giovanni DI GUARDO <g.diguardo@cineca.it>
2024-05-05 17:34:33 +0200

    Dependency Injection Using Spring

--- src/main/java/com/giovdigua/spring6di/controllers/SetInjectedController.java
+++ src/main/java/com/giovdigua/spring6di/controllers/SetInjectedController.java
@@ -1,12 +1,18 @@
 package com.giovdigua.spring6di.controllers;
 
 import com.giovdigua.spring6di.services.GreetingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 
+@Controller
 public class SetInjectedController {
+
 
     private GreetingService greetingService;
 
+    @Autowired
     public void setGreetingService(GreetingService greetingService) {
+        System.out.println("SetInjectedController.setGreetingService");
         this.greetingService = greetingService;
     }
 
--- src/test/java/com/giovdigua/spring6di/controllers/SetInjectedControllerTest.java
+++ src/test/java/com/giovdigua/spring6di/controllers/SetInjectedControllerTest.java
@@ -3,18 +3,16 @@ package com.giovdigua.spring6di.controllers;
 import com.giovdigua.spring6di.services.GreetingServiceImpl;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@SpringBootTest
 class SetInjectedControllerTest {
 
+    @Autowired
     SetInjectedController setInjectedController;
-    @BeforeEach
-    void setUp() {
-        setInjectedController = new SetInjectedController();
-        //Important in setter if forget setting injection we have null pint exception
-        setInjectedController.setGreetingService(new GreetingServiceImpl());
-    }
 
     @Test
     void sayHello() {
